6d40c02c6e491264c6a7d4087ef7b84ab98878d1
이덕호/TOAST클라이언트개발팀/NE <deokho.lee@nhnent.com>
2019-05-17 19:09:54 +0900

    Chapter 15. Chanllenge : ShareCompat

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -131,12 +132,13 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
-                i = Intent.createChooser(i, getString(R.string.send_report)); // 매번 선택기가 나오도록
-                startActivity(i);
+                ShareCompat.IntentBuilder intentBuilder = ShareCompat.IntentBuilder.from(getActivity());
+                intentBuilder.setType("text/plain");
+                intentBuilder.setSubject(getString(R.string.crime_report_subject));
+                intentBuilder.setText(getCrimeReport());
+                intentBuilder.createChooserIntent();
+                intentBuilder.setChooserTitle(getString(R.string.send_report));
+                intentBuilder.startChooser();
             }
         });
 
